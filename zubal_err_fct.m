f91ad36f3c1fc6b054ed80a4158301606f1301ca
*David <*david.da389@gmail.com*>
2022-06-16 14:27:17 -0400

    updates in the description

--- zubal_err_fct.m
+++ zubal_err_fct.m
@@ -1,20 +1,20 @@
-function [mean_abs_error_method] = zubal_err_fct(mask_file_name, method, mag_field_sim, plane, cross_sect)
+function [mean_rel_error_method] = zubal_err_fct(mask_file_name, method, mag_field_sim, plane, cross_sect)
 %
-% zubal_err_fct computes the mean absolute error for a certain mask in the zubal
+% zubal_err_fct computes the mean relative error for a certain mask in the zubal
 % phantom. First, the mask is applied on the true magnetic field and on the
 % method you chose. Then, we substract the B0 map created from your method
-% and the true magnetic field and finally we divide tis substraction with 
+% and the true magnetic field and finally we divide this operation with 
 % the true magnetic (element by element). If you have a 3D data set 
 % (256x256x128), you have to choose a plane in the matrix, the first 
 % dimension is for the sagital plane, the second dimension is for the
 % coronal plane and the third dimension is for the axial plane. You'll now
-% have a 2D data set containing the absolute error for each element of your
+% have a 2D data set containing the relative error for each element of your
 % matrix. We calculate the mean value of the 2D data set and the output 
-% is the mean absolute error for a specify plane and cross section.
+% is the mean relative error for a specify plane and cross section.
 %
 % _SYNTAX_
 % 
-% [mean_abs_error_method] = err_fct(mask_file_name, method, mag_field_sim, plane, cross_sect)
+% [mean_rel_error_method] = err_fct(mask_file_name, method, mag_field_sim, plane, cross_sect)
 %
 % _DESCRIPTION_
 %
@@ -42,8 +42,8 @@ function [mean_abs_error_method] = zubal_err_fct(mask_file_name, method, mag_fie
 %
 % _OUTPUTS_
 %
-%    mean_abs_error_method 
-%      mean absolute error for a specify plane and cross section
+%    mean_rel_error_method 
+%      mean relative error for a specify plane and cross section
 % 
 %
 % _EXAMPLE_
@@ -58,7 +58,7 @@ mask = niftiread(mask_file_name);  % mask in the sinuses
 mask_dBz = mask .* 1e6 .* real(mag_field_sim); % ROI for the real magnetic field
 mask_method = mask .* method; % ROI for dual or multi echo
 
-% calculating the efficacity of both methods using the absolute error
+% calculating the efficacity of both methods using the relative error
 switch plane
     
     case 'sagital'
@@ -66,42 +66,42 @@ switch plane
         for i = 1 : dim2
             for j = 1 : dim3
                 if mask_dBz(cross_sect,i,j)==0
-                    abs_error_method(i,j) = 0;
+                    rel_error_method(i,j) = 0;
                 else
-                    abs_error_method(i,j) = abs((mask_dBz(cross_sect,i,j) - mask_method(cross_sect,i,j))/...
-                                            mask_dBz(cross_sect,i,j)); % absolute error 
+                    rel_error_method(i,j) = abs((mask_dBz(cross_sect,i,j) - mask_method(cross_sect,i,j))/...
+                                            mask_dBz(cross_sect,i,j)); % relative error  
                 end
             end
         end
-        mean_abs_error_method = sum(abs_error_method(:,:), 'all')/nnz(mask(cross_sect,:,:));
+        mean_rel_error_method = sum(rel_error_method(:,:), 'all')/nnz(mask(cross_sect,:,:));
         
     case 'coronal'
         
         for i = 1 : dim1
             for j = 1 : dim3
                 if mask_dBz(i,cross_sect,j)==0
-                    abs_error_method(i,j) = 0;
+                    rel_error_method(i,j) = 0;
                 else
-                    abs_error_method(i,j) = abs((mask_dBz(i,cross_sect,j) - mask_method(i,cross_sect,j))/...
-                                            mask_dBz(i,cross_sect,j)); % absolute error
+                    rel_error_method(i,j) = abs((mask_dBz(i,cross_sect,j) - mask_method(i,cross_sect,j))/...
+                                            mask_dBz(i,cross_sect,j)); % relative error 
                 end
             end
         end
-        mean_abs_error_method = sum(abs_error_method(:,:), 'all')/nnz(mask(:,cross_sect,:));
+        mean_rel_error_method = sum(rel_error_method(:,:), 'all')/nnz(mask(:,cross_sect,:));
         
     case 'axial'
         
         for i = 1 : dim1
             for j = 1 : dim2
                 if mask_dBz(i,j,cross_sect)==0
-                abs_error_method(i,j) = 0;
+                rel_error_method(i,j) = 0;
                 else
-                abs_error_method(i,j) = abs((mask_dBz(i,j,cross_sect) - mask_method(i,j,cross_sect))/...
-                                        mask_dBz(i,j,cross_sect)); % absolute error 
+                rel_error_method(i,j) = abs((mask_dBz(i,j,cross_sect) - mask_method(i,j,cross_sect))/...
+                                        mask_dBz(i,j,cross_sect)); % relative error 
                 end
             end
         end
-        mean_abs_error_method = sum(abs_error_method(:,:), 'all')/nnz(mask(:,:,cross_sect));
+        mean_rel_error_method = sum(rel_error_method(:,:), 'all')/nnz(mask(:,:,cross_sect));
     
     otherwise
         
