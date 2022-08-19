fd294bfd6781bdaed4222b9cb2ee23db5932f137
MathildeDupouy <82039222+MathildeDupouy@users.noreply.github.com>
2022-08-19 14:25:54 -0400

    Update startup.m

--- startup.m
+++ startup.m
@@ -1,6 +1,13 @@
 addpath 'shim-sim_matlab'
+addpath 'shim-sim_matlab/misc'
+addpath 'shim-sim_matlab/external/NIFTI'
+addpath 'shim-sim_matlab/numerical_model'
 addpath 'phantom'
 addpath 'masks'
 
-% make sure to add the zubal_EAO.nii file in your repo 
-addpath ../zubal_EAO.nii
+% Complete this path if Fourier-based-field-estimation is not directly 
+% in the precedent directory
+addpath '../Fourier-based-field-estimation' 
+% Complete this path to add Zubal_EAO file (previously donloaded) to the
+% path
+addpath 'Zubal_EAO.nii' 
